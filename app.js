8fca9754f51c2393352202647aecf6b1567b37d9
Narmeen alkatab <narmeen.kattab1@gmail.com>
2023-03-01 08:04:49 +0000

    pop-up by js

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ let name = prompt("What is your name?");
 
 let gender = prompt("What is your gender?");
 
+if (gender !== "male" && "female") {
+  alert(" please enter  male or female");
+}
 
-let age= prompt("What is your age?");
+let age = prompt("What is your age?");
 
-age <= 0 ? alert("THERE IS SOME THING WRONGE. the is not your real age. ")  :
+age <= 0
+  ? alert("THERE IS SOME THING WRONGE. the is not your real age. ")
+  : confirm("if you want to skip welcoming message, you can press CANCEL");
 
-confirm("if you want to skip welcoming message, you can press CANCEL")
-
-
-gender == "female"  ? alert("welcome Ms " + name): gender == "male"  ? alert("welcome Mr " + name):  alert("welcome " + name);
-
-
-
+gender == "female"
+  ? alert("welcome Ms " + name)
+  : gender == "male"
+  ? alert("welcome Mr " + name)
+  : alert("welcome " + name);
